61bb08377147af354f52946eb0dbe8407a48a3f8
santhosh <santhosh@MSI>
2025-06-26 15:34:53 +0530

    add

---
+++ demo21/src/main/java/com/fileutility/filehandling.java
@@ -0,0 +1,10 @@
+package com.fileutility;
+
+public class filehandling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ demo21/src/test/java/com/createproduct/createproduct.java
@@ -0,0 +1,10 @@
+package com.createproduct;
+
+public class createproduct {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
